b0589d0c0510fda5107479eb121f981a5814f290
Guilherme C. Pilon <guipilon@gmail.com>
2021-12-23 20:41:19 -0300

    first commit

---
+++ HttpClientResilient/HttpClientResilient.cs
@@ -0,0 +1,16 @@
+﻿namespace HttpClientResilient
+{
+    public partial class HttpClientResilient : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        public HttpClientResilient() 
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
---
+++ HttpClientResilient/HttpClientResilient_Props.cs
@@ -0,0 +1,11 @@
+﻿namespace HttpClientResilient
+{
+    public partial class HttpClientResilient
+    {
+        public Uri? BaseAddress 
+        { 
+            get { return _httpClient.BaseAddress;  }
+            set { _httpClient.BaseAddress = value; }
+        }
+    }
+}
