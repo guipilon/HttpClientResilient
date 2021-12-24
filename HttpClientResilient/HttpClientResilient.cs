8408b2b307b0f7772e5d3606247dfd073477b0a5
Guilherme C. Pilon <guipilon@gmail.com>
2021-12-24 10:56:49 -0300

    add retryPolicy

--- HttpClientResilient/HttpClientResilient.cs
+++ HttpClientResilient/HttpClientResilient.cs
@@ -1,11 +1,24 @@
-﻿namespace HttpClientResilient
+﻿using Polly;
+using Polly.Retry;
+
+namespace HttpClientResilient
 {
     public partial class HttpClientResilient : IDisposable
     {
         private readonly HttpClient _httpClient;
+        private AsyncRetryPolicy _retryPolicy;
+
         public HttpClientResilient() 
         {
             _httpClient = new HttpClient();
+            _retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(2, retryAttempt => {
+                    var timeToWait = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+                    Console.WriteLine($"Waiting {timeToWait.TotalSeconds} seconds");
+                    return timeToWait;
+                }
+                );
         }
 
         public void Dispose()
