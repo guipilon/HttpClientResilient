8e92506981168bf6e815a2d3af0656b586b70a4f
Guilherme C. Pilon <guipilon@gmail.com>
2021-12-24 10:05:56 -0300

    Adding tests

---
+++ HttpClientResilient.Integration.Test/HttpClientResilientTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientResilient.Integration.Test
+{
+    [TestClass]
+    public class HttpClientResilientTests
+    {
+        [TestMethod]
+        public void MakeGetRequest_Successful()
+        {
+            HttpClientResilient client = new HttpClientResilient();
+
+            HttpRequestMessage request = new HttpRequestMessage();
+
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new System.Uri("https://httpbin.org/ip");
+
+            var response = client.SendResilient(request);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task MakeGetRequestAsync_SuccessfulAsync()
+        {
+            HttpClientResilient client = new HttpClientResilient();
+
+            HttpRequestMessage request = new HttpRequestMessage();
+
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new System.Uri("https://httpbin.org/ip");
+
+            var response = await client.SendAsyncResilient(request);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ HttpClientResilient/HttpClientResilient_Methods.cs
@@ -0,0 +1,19 @@
+﻿namespace HttpClientResilient
+{
+    public partial class HttpClientResilient
+    {
+        public HttpResponseMessage SendResilient(HttpRequestMessage request)
+        {
+            var response = _httpClient.Send(request);
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> SendAsyncResilient(HttpRequestMessage request)
+        {
+            var result = await _httpClient.SendAsync(request);
+
+            return result;
+        }
+    }
+}
